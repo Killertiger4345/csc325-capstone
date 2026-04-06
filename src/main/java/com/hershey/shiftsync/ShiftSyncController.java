b2f159c2ee5dfe8442ae10b905aa241f75b68a53
Harshdeep Singh <singh36@farmingdale.edu>
2026-04-06 14:28:07 -0400

    Setting up test application using Google Firebase Database

--- src/main/java/com/hershey/shiftsync/ShiftSyncController.java
+++ src/main/java/com/hershey/shiftsync/ShiftSyncController.java
@@ -8,13 +8,13 @@ import javafx.scene.control.TextField;
 
 public class ShiftSyncController {
     @FXML
-    private Label welcomeText;
-
-    @FXML
     private TextField emailText;
 
     @FXML
     private PasswordField passwordText;
+
+    @FXML
+    private Label errorText;
 
     @FXML
     private Button register;
@@ -24,7 +24,15 @@ public class ShiftSyncController {
 
     @FXML
     protected void onRegisterButtonClick() {
-        
+        if (emailText.getText().isEmpty() || passwordText.getText().isEmpty()) {
+            errorText.setText("ERROR: Email or password empty");
+        }
+        else {
+            /* Add a try/catch block to check for email and password requirements */
+            System.out.println("Register button clicked");
+            System.out.println("TODO: Register with email " + emailText.getText());
+            System.out.println("TODO: Register with password " + passwordText.getText());
+        }
     }
 
     @FXML
