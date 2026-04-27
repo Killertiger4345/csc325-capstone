13072c9049fbf994df9f402189e13ff1083cebcd
nwodco-boop <nwodco@farmingdale.edu>
2026-04-26 21:56:41 -0400

    Add files via upload

--- src/main/java/com/hershey/shiftsync/Launcher.java
+++ src/main/java/com/hershey/shiftsync/Launcher.java
@@ -1,9 +1,9 @@
-package com.hershey.shiftsync;
+package com.example.calendar;
 
 import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(ShiftSync.class, args);
+        Application.launch(com.example.calendar.HelloApplication.class, args);
     }
 }
