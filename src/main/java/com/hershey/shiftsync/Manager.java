7455c467f9e0cc8b6ef5c5d3d627b7d196152867
ciulbg <ciulbg@farmingdale.edu>
2026-05-01 17:14:14 -0400

    Add first and last name to Person and Manager class

--- src/main/java/com/hershey/shiftsync/Manager.java
+++ src/main/java/com/hershey/shiftsync/Manager.java
@@ -2,6 +2,10 @@ package com.hershey.shiftsync;
 
 public class Manager extends Person {
 
+    public Manager(String email, String password, String phoneNumber, String firstName, String lastName) {
+        super(email, password, phoneNumber, firstName, lastName);
+    }
+
     public Manager(String email, String password, String phoneNumber) {
         super(email, password, phoneNumber);
     }
--- src/main/java/com/hershey/shiftsync/Person.java
+++ src/main/java/com/hershey/shiftsync/Person.java
@@ -5,7 +5,8 @@ public class Person {
     private String email;
     private String password;
     private String phoneNumber;
-
+    private String firstName;
+    private String lastName;
 
     // Constructors
     public Person(String email,String password) {
@@ -19,11 +20,20 @@ public class Person {
         this.phoneNumber = phoneNumber;
     }
 
+    public Person(String email, String password, String phoneNumber, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
     // getter methods
+
     public String getEmail() {
         return email;
     }
-
     public String getPassword() {
         return password;
     }
@@ -32,17 +42,31 @@ public class Person {
         return phoneNumber;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
     // setter method
+
     public void setEmail(String email) {
         this.email = email;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
 
     public void setPhonenumber(String phonenumber) {
         this.phoneNumber = phonenumber;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
 
