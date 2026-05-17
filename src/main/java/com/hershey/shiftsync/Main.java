7b8d9fd2248e4b0d0895804b5784c8def0770a14
Chigozie <Nwodco@farmingdale.edu>
2026-05-16 20:39:10 -0400

    new push for calendar

--- src/main/java/com/hershey/shiftsync/Main.java
+++ src/main/java/com/hershey/shiftsync/Main.java
@@ -10,13 +10,8 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-<<<<<<<< HEAD:src/main/java/com/hershey/shiftsync/Main.java
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Calendar.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-========
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Calander.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
->>>>>>>> origin/Calendar:src/main/java/com/hershey/shiftsync/HelloApplication.java
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
